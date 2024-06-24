830165db6fa279d302744a7e5beb1fc9eb5bd43e
Ravi Gautam <rvgautam59@gmail.com>
2024-06-24 15:16:21 +0530

    new style added

--- src/components/EventDetails/EventDetails.jsx
+++ src/components/EventDetails/EventDetails.jsx
@@ -5,12 +5,18 @@ function EventDetails() {
   return (
     <div className="event_details">
       <div className="breakdown">
-        <h1>BREAKDOWN ANALYSIS</h1>
-        <h1>DECISION</h1>
+        <h1>
+          BREAKDOWN <span className="analysis">ANALYSIS</span>
+        </h1>
+        <h1 className="decision">DECISION</h1>
       </div>
       <img src="/src/assets/event.jpg" alt="Event" />
-      <h2>Event Venue - IPS Academy Indore </h2>
-      <p>Event Date - 08 August 2024</p>
+      <p>
+        <b>Event Venue</b> - Audi-2 IPS Academy Rajendra Nagar, Indore
+      </p>
+      <p>
+        <b>Event Date</b> - 08 August 2024
+      </p>
       <button>Register</button>
     </div>
   );
