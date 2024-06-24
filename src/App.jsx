2ea302cc0f6a9ff9814f71b577127c6b71cb8fb1
shivanshi <guptashivanshi250@gmail.com>
2024-06-24 21:20:42 +0530

    Mergeed  about section sucessfully

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import About from './component/AboutUs/About'
 import Footer from './component/Footer/Footer'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <>
+    <About/>
      <Footer/>
     </>
   )
--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
    <>
 
-+
+
 
     <footer id="footer">
       <div className="container ">
