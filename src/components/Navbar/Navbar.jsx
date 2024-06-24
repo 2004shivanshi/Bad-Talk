f06a7e42ccb16c396257599303b0be3746aa8077
Ravi Gautam <rvgautam59@gmail.com>
2024-06-24 20:34:29 +0530

    new style added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,13 +14,11 @@ function Navbar() {
         <img src="/src/assets/Vector1.png" alt="Logo" />
       </div>
       <div className={`Nav_center ${isOpen ? "open" : ""}`}>
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Tracks</li>
-          <li>Our History</li>
-          <li>Contact Us</li>
-        </ul>
+        <a href="#home">Home</a>
+        <a href="#about">About Us</a>
+        <a href="#tracks">Tracks</a>
+        <a href="#history">Our History</a>
+        <a href="#contact">Contact Us</a>
       </div>
       <div className="Nav_right">
         <button>Register</button>
